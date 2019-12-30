e0357768e2abccd5f94ff7629b65a7c57035b57a
Ian Yong <ianyongyc@gmail.com>
2019-12-30 18:27:24 +0800

    Add colour to refresh animation

--- frontend/app/src/main/java/com/github/iwle/ausm/ReviewFragment.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/ReviewFragment.kt
@@ -29,6 +29,8 @@ class ReviewFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         val v = inflater.inflate(R.layout.fragment_review, container, false)
         swipeRefreshLayout = v.findViewById(R.id.swipe_refresh_layout)
         swipeRefreshLayout.setOnRefreshListener(this)
+        // Set refresh animation colour cycle
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimaryDark, R.color.colorAccent)
         fetchData()
         recyclerView = v.findViewById<RecyclerView>(R.id.recycler_view).apply {
             layoutManager = LinearLayoutManager(this.context)
@@ -44,6 +46,9 @@ class ReviewFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         data.add(Establishment("https://media.licdn.com/dms/image/C5103AQGoTP2bX1HwYA/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=E-2pisrSdORszvYoWZfoVH9QRnIbpK8vGALFoz_WCT0", "Ong Wai Boon", "Bedok", 0.0))
         data.add(Establishment("https://www.comp.nus.edu.sg/stfphotos/sooyj_2.jpg", "Soo Yuen Jien", "NUS", 5.0))
         data.add(Establishment("https://media-cdn.tripadvisor.com/media/photo-s/02/39/56/bd/side-entrance.jpg", "Starbucks","Plaza Singapura", 4.1))
+        data.add(Establishment("https://images-na.ssl-images-amazon.com/images/I/81zfHiBLQBL._SX466_.jpg", "Another Pomeranian", "lorem ipsum", 4.9))
+        data.add(Establishment("https://images-na.ssl-images-amazon.com/images/I/81zfHiBLQBL._SX466_.jpg", "Another Pomeranian", "lorem ipsum", 4.9))
+        data.add(Establishment("https://images-na.ssl-images-amazon.com/images/I/81zfHiBLQBL._SX466_.jpg", "Another Pomeranian", "lorem ipsum", 4.9))
         data.add(Establishment("https://images-na.ssl-images-amazon.com/images/I/81zfHiBLQBL._SX466_.jpg", "Another Pomeranian", "lorem ipsum", 4.9))
         reviewListAdapter = ReviewListAdapter(data)
 
