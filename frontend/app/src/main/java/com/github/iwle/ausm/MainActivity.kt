1990532c83c654b1c5a996f0932fe527ccf40000
Ian Yong <ianyongyc@gmail.com>
2020-01-01 01:01:30 +0800

    Hide extended FAB on toolbar collapse

--- frontend/app/src/main/java/com/github/iwle/ausm/MainActivity.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.tabs.TabLayout
+import kotlin.math.abs
 
 class MainActivity : FragmentActivity() {
     private lateinit var tabLayout: TabLayout
@@ -14,26 +15,28 @@ class MainActivity : FragmentActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var appBarLayout: AppBarLayout
     private lateinit var floatingActionButton: ExtendedFloatingActionButton
+    private var isFabEnabled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initialiseTabPagerAdapter()
-    }
-
-    private fun initialiseTabPagerAdapter() {
         tabLayout = findViewById(R.id.tab_layout)
         viewPager = findViewById(R.id.tab_view_pager)
         toolbar = findViewById(R.id.toolbar)
         appBarLayout = findViewById(R.id.app_bar_layout)
         floatingActionButton = findViewById(R.id.extended_floating_action_button)
+
+        initialiseTabPagerAdapter()
+        initialiseFloatingActionButton()
+    }
+
+    private fun initialiseTabPagerAdapter() {
         val tabFragmentList = listOf(MapFragment(), ReviewFragment())
         val tabNameList = listOf(getString(R.string.tab_gps), getString(R.string.tab_reviews))
         val tabPagerAdapter = TabPagerAdapter(this, tabFragmentList)
         viewPager.adapter = tabPagerAdapter
-        tabLayout.addOnTabSelectedListener(object :
-            TabLayout.OnTabSelectedListener {
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 viewPager.currentItem = tab.position
                 // Show app bar when tab is selected
@@ -54,8 +57,8 @@ class MainActivity : FragmentActivity() {
                 toolbar.title = tabNameList[position]
                 // Show extended FAB only on Reviews tab
                 when(position) {
-                    0 -> floatingActionButton.hide()
-                    1 -> floatingActionButton.show()
+                    0 -> updateFabStatus(false)
+                    1 -> updateFabStatus(true)
                 }
             }
         })
@@ -63,5 +66,37 @@ class MainActivity : FragmentActivity() {
         viewPager.isUserInputEnabled = false
         // Set initial toolbar title
         toolbar.title = tabNameList[0]
+    }
+
+    private fun initialiseFloatingActionButton() {
+        // Hide extended FAB when toolbar is collapsed
+        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if(abs(verticalOffset) - appBarLayout!!.totalScrollRange == 0) {
+                // Collapsed
+                hideFab()
+            } else {
+                // Expanded
+                showFab()
+            }
+        })
+    }
+
+    private fun updateFabStatus(status: Boolean) {
+        isFabEnabled = status
+        if(isFabEnabled) {
+            showFab()
+        } else {
+            hideFab()
+        }
+    }
+
+    private fun showFab() {
+        if(isFabEnabled) {
+            floatingActionButton.show()
+        }
+    }
+
+    private fun hideFab() {
+        floatingActionButton.hide()
     }
 }
