0269348d58a57c2bfa999a9594bdf58ea70ffc7e
Ian Yong <ianyongyc@gmail.com>
2020-01-02 01:27:18 +0800

    Add login Firebase authentication

--- frontend/app/src/main/java/com/github/iwle/ausm/LoginActivity.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.github.iwle.ausm
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : Activity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        emailEditText = findViewById(R.id.email_edit_text)
+        passwordEditText = findViewById(R.id.password_edit_text)
+        loginButton = findViewById(R.id.login_button)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            val email: String = emailEditText.text.toString()
+            val password: String = passwordEditText.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(this, getString(R.string.login_field_missing), Toast.LENGTH_LONG).show()
+            } else {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, getString(R.string.login_success), Toast.LENGTH_LONG).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, getString(R.string.login_failure), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
+
+
 }
