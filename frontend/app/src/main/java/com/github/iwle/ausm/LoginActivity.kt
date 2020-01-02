c6da1a984a2c7b86824a649dd87dcde15de542d6
Ian Yong <ianyongyc@gmail.com>
2020-01-02 15:26:22 +0800

    Make EditText lose focus when clicking outside

--- frontend/app/src/main/java/com/github/iwle/ausm/LoginActivity.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.github.iwle.ausm
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : Activity() {
     private lateinit var emailEditText: EditText
@@ -45,5 +52,21 @@ class LoginActivity : Activity() {
         }
     }
 
-
+    // Hide keyboard when focus is lost
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
 }
