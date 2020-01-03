74f21abebbee29514c2df59557d8fb8a7b7c40b3
Ian Yong <ianyongyc@gmail.com>
2020-01-03 17:38:09 +0800

    Force hide keyboard when TextArea loses focus

--- frontend/app/src/main/java/com/github/iwle/ausm/AddReviewActivity.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/AddReviewActivity.kt
@@ -1,7 +1,11 @@
 package com.github.iwle.ausm
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.View
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -18,5 +22,23 @@ class AddReviewActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    // Hide keyboard when focus is lost
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
