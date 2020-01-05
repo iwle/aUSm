9de6f8d10037946193e2e57c8efff6e6464e72ca
Ian Yong <ianyongyc@gmail.com>
2020-01-05 15:32:55 +0800

    Populate EditReviewActivity fields
    When editing a review, the form for editing is initialised with the
    current values.

--- frontend/app/src/main/java/com/github/iwle/ausm/EditReviewActivity.kt
+++ frontend/app/src/main/java/com/github/iwle/ausm/EditReviewActivity.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.github.iwle.ausm.model.Review
+import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.textfield.TextInputEditText
 import me.zhanghai.android.materialratingbar.MaterialRatingBar
@@ -17,7 +18,17 @@ import me.zhanghai.android.materialratingbar.MaterialRatingBar
 class EditReviewActivity : AppCompatActivity() {
     private lateinit var review: Review
     private lateinit var noiseChipGroup: ChipGroup
+    private lateinit var noiseChip1: Chip
+    private lateinit var noiseChip2: Chip
+    private lateinit var noiseChip3: Chip
+    private lateinit var noiseChip4: Chip
+    private lateinit var noiseChip5: Chip
     private lateinit var crowdChipGroup: ChipGroup
+    private lateinit var crowdChip1: Chip
+    private lateinit var crowdChip2: Chip
+    private lateinit var crowdChip3: Chip
+    private lateinit var crowdChip4: Chip
+    private lateinit var crowdChip5: Chip
     private lateinit var addReviewButton: Button
     private lateinit var overallRatingBar: MaterialRatingBar
     private lateinit var reviewTextEdit: TextInputEditText
@@ -29,13 +40,24 @@ class EditReviewActivity : AppCompatActivity() {
         review = intent.getSerializableExtra("review") as Review
 
         noiseChipGroup = findViewById(R.id.noise_chip_group)
+        noiseChip1 = findViewById(R.id.noise_chip_1)
+        noiseChip2 = findViewById(R.id.noise_chip_2)
+        noiseChip3 = findViewById(R.id.noise_chip_3)
+        noiseChip4 = findViewById(R.id.noise_chip_4)
+        noiseChip5 = findViewById(R.id.noise_chip_5)
         crowdChipGroup = findViewById(R.id.crowd_chip_group)
+        crowdChip1 = findViewById(R.id.crowd_chip_1)
+        crowdChip2 = findViewById(R.id.crowd_chip_2)
+        crowdChip3 = findViewById(R.id.crowd_chip_3)
+        crowdChip4 = findViewById(R.id.crowd_chip_4)
+        crowdChip5 = findViewById(R.id.crowd_chip_5)
         addReviewButton = findViewById(R.id.edit_review_button)
         overallRatingBar = findViewById(R.id.overall_rating_bar)
         reviewTextEdit = findViewById(R.id.review_text_input)
         toolbar = findViewById(R.id.edit_review_toolbar)
 
         initialiseToolbar()
+        populateData()
     }
 
     private fun initialiseToolbar() {
@@ -44,6 +66,25 @@ class EditReviewActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun populateData() {
+        when(review.noiseRating) {
+            1 -> noiseChip1.isSelected = true
+            2 -> noiseChip2.isSelected = true
+            3 -> noiseChip3.isSelected = true
+            4 -> noiseChip4.isSelected = true
+            5 -> noiseChip5.isSelected = true
+        }
+        when(review.crowdRating) {
+            1 -> crowdChip1.isSelected = true
+            2 -> crowdChip2.isSelected = true
+            3 -> crowdChip3.isSelected = true
+            4 -> crowdChip4.isSelected = true
+            5 -> crowdChip5.isSelected = true
+        }
+        overallRatingBar.rating = review.overallRating.toFloat()
+        reviewTextEdit.setText(review.review)
     }
 
     // Hide keyboard when focus is lost
